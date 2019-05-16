be6f40d0035cac0bcc8196736afeada2cf667c57
Parthasarathi RV <37892718+p0rth01998@users.noreply.github.com>
2019-05-16 11:06:01 +0530

    Add files via upload

---
+++ alphabetrnot.py
@@ -0,0 +1,5 @@
+v=input("Enter a letter: ")
+if (ord(v)>=65 and ord(v)<=90) or (ord(v)>=97 and ord(v)<=122 ) :
+	print("The Entered letter is a alphabet")
+else:
+	print("The Entered letter is not a alphabet")
