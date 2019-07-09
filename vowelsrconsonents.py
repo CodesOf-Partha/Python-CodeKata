91ff0f7aa7ca8948694c5b45406d2308816c8f42
Parthasarathi RV <37892718+p0rth01998@users.noreply.github.com>
2019-07-09 14:42:36 +0530

    Update vowelsrconsonents.py

--- vowelsrconsonents.py
+++ vowelsrconsonents.py
@@ -1,4 +1,4 @@
-v=input("Enter a letter: ")
+v=input()
 if (ord(v)>=65 and ord(v)<=90) or (ord(v)>=97 and ord(v)<=122 ) :
 	if v=='a' or v=='e' or v=='i' or v=='o' or v=='u' or v=='A' or v=='E' or v=='I' or v=='O' or v=='U':
 		print("Vowel")
