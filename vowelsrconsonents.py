888820fba008441e7ffd26380519342b563d3847
Parthasarathi RV <37892718+p0rth01998@users.noreply.github.com>
2019-07-09 14:42:16 +0530

    Update vowelsrconsonents.py

--- vowelsrconsonents.py
+++ vowelsrconsonents.py
@@ -1,8 +1,8 @@
 v=input("Enter a letter: ")
 if (ord(v)>=65 and ord(v)<=90) or (ord(v)>=97 and ord(v)<=122 ) :
 	if v=='a' or v=='e' or v=='i' or v=='o' or v=='u' or v=='A' or v=='E' or v=='I' or v=='O' or v=='U':
-		print("vowels")
+		print("Vowel")
 	else:
-		print("Consonent")
+		print("Consonant")
 else:
-	print("Invalid")
+	print("invalid")
