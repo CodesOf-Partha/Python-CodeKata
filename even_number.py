179b9847c8a5bb3eecf2fd98f6b290410cc15124
Parthasarathi RV <37892718+p0rth01998@users.noreply.github.com>
2019-07-09 15:23:11 +0530

    Update even_number.py

--- even_number.py
+++ even_number.py
@@ -3,7 +3,7 @@ start,end=input().split()
 string=""
 start=int(start)
 end=int(end)
-for i in range(start+1,end+1):
+for i in range(start+1,end):
 	if i%2==0:
 		string=string+str(i)+" "
 print(string)
