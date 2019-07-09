5892d919d915ce50034876b26384946b207fdcae
Parthasarathi RV <37892718+p0rth01998@users.noreply.github.com>
2019-07-09 16:34:45 +0530

    Add files via upload

---
+++ pro_longestCommonString.py
@@ -0,0 +1,11 @@
+def longest(str1,str2):
+        if(str1 in str2):
+            return str1
+        else:
+            return checkLongest(str1[0:len(str1)-1],str2)
+inputSize = int(input())
+inputList= []
+for _ in range(0,inputSize):
+    inputList.append(input())
+inputList.sort()
+print(longest(inputList[0],inputList[inputSize-1]))
