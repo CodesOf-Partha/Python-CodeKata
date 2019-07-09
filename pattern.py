215bbb97af8d81bffef6e157516d3845e6f45a6d
Parthasarathi RV <37892718+p0rth01998@users.noreply.github.com>
2019-07-09 13:05:49 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+num ,x = input().split()
+x = int(x)
+sumNum = []
+comb = combinations(num,len(num)-x)
+for i in comb:
+    sumNum.append("".join(i))
+print(min(sumNum))
